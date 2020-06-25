a2d552a10287bf54c8173b9edded98ab72946840
neverjunior <67434404+neverjunior@users.noreply.github.com>
2020-06-25 13:59:30 -0400

    Update Tree.py

--- Tree.py
+++ Tree.py
@@ -1,14 +1,1 @@
-import os
 
-def abhi( path , fil  , depth):
-      print('|'+depth*"--" ,fil)
-      if os.path.isdir(path):
-         for obj in os.listdir(path):            
-        
-            abhi(path+'/' +obj, obj , depth+1 )
-           
-        
-
-
-
-abhi("/home/neverjunior/Videos" , "Videos" , 0 )                          
